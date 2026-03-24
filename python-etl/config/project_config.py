4bb7a5c5e24428476cdc347ad47efb7c3d819d11
Shaoya Zhang <shaoya.zhang01@estudiant.upf.edu>
2026-03-24 20:55:05 +0100

    update

--- python-etl/config/project_config.py
+++ python-etl/config/project_config.py
@@ -1,6 +1,17 @@
 import time
-
 log_root_path = "D:/Pythons_studies/logs/"
 log_filename = f'pyetl-{time.strftime("%Y%m%d-%H", time.localtime(time.time()))}.log'
 
 level = 10
+
+
+
+##################### MySQLUtil配置 ##########################
+mysql_charset = 'utf8' # utf8不是utf-8
+##################### 原数据库配置 start ###########################
+metadata_host = 'localhost'
+metadata_port = 3306 # 一定是整型
+metadata_user = 'root'
+metadata_password = 'Alph@2025'
+metadata_database = 'metadata' # 当前处理的是元数据，所以可以先写metadata
+##################### 原数据库配置 end ###########################
--- python-etl/util/mysql_util.py
+++ python-etl/util/mysql_util.py
@@ -11,27 +11,67 @@
 '''
 
 import pymysql
+import os
+import sys
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(CURRENT_DIR)
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+from util.logging_util import init_logger
+import config.project_config as config
+logger = init_logger()
 class MySQLUtil:
-    def __init__(self):
+    def __init__(self, 
+                host = config.metadata_host, 
+                port = config.metadata_port, # 一定是整型
+                user = config.metadata_user,
+                password = config.metadata_password,
+                charset = config.mysql_charset, # utf8不是utf-8
+                database = config.metadata_database, # 当前处理的是元数据，所以可以先写metadata
+                autocommit = False  # 默认无自动提交
+                ):
         ''' 
         创建MySQL的链接
         '''
-        host = 'localhost',
-        port = 3306, # 一定是整型
-        user = 'root',
-        password = 'Alph@2025',
-        charset = 'utf8' # utf8不是utf-8
+        self.conn = pymysql.connect(
+            host = host,
+            port = port, 
+            user = user,
+            password = password,
+            charset = charset, 
+            database = database, 
+            autocommit = autocommit 
+        )
+        if self.conn:
+            logger.info('数据库链接建立成功')
       
     def query(self, sql):
-        pass
-    def select_db(self, db):
+        """_summary_
+
+        Args:
+            sql (_type_): 需要执行的查询语句
+            return: 查询结果
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        logger.info(result)
+        return result
+    
+    
+    def select_db(self, db_name):
         """_summary_
 
         Args:
             db (数据库): 需要切换的数据库
             return: None
         """
+        # cursor = self.conn.cursor()
+        # cursor.execute('use' + db_name + ';')
+        self.conn.select_db(db_name)
         
     def execute_with_autocommit(self, sql):
         """_summary_
@@ -67,4 +107,19 @@ class MySQLUtil:
         pass
     
     def close(self):
-        pass
+        if self.conn:
+            self.conn.close()
+            self.conn = None # 防御性编程
+    
+    
+    
+if __name__ == '__main__':
+    mysql_util = MySQLUtil()
+    # mysql_util.close()
+    # query_result = mysql_util.query('select* from test')
+    # logger.info(query_result)
+    mysql_util.query('select* from test')
+    mysql_util.select_db('retail')
+    mysql_util.query('SELECT database()')   # 查询现在的数据库
+    mysql_util.close()
+    
